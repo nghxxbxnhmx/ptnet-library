29bdb6f6fddd7bbeab107e61229c0bea0155280c
Zion Nguyễn <94814450+hungiter@users.noreply.github.com>
2024-06-17 06:23:17 +0700

    Wifi Scan Updated + Minor fix @SuppressLint("MissingPermission") - Fix Build Failed because of missing wifi permission + Add more advanced wifi features

--- shared/src/androidMain/kotlin/com/ftel/ptnetlibrary/services/WifiScanService.android.kt
+++ shared/src/androidMain/kotlin/com/ftel/ptnetlibrary/services/WifiScanService.android.kt
@@ -1,7 +1,10 @@
 package com.ftel.ptnetlibrary.services
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
+import android.os.Build
 import com.ftel.ptnetlibrary.dto.WifiScanResultDTO
 import com.ftel.ptnetlibrary.utils.getAppContext
 
@@ -9,35 +12,34 @@ actual class WifiScanService {
     private val wifiManager =
         getAppContext().applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
+    @SuppressLint("MissingPermission")
     actual fun getScanResult(): List<WifiScanResultDTO> {
 
         if (!wifiManager.isWifiEnabled) {
             return emptyList()
         }
+
         val wifiScanResults = wifiManager.scanResults
 
         val wifiScanResultList = mutableListOf<WifiScanResultDTO>()
 
-//        if (ContextCompat.checkSelfPermission(
-//                getAppContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(
-//                getAppContext() as Activity,
-//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-//                1009
-//            )
-//            return emptyList()
-//        }
-
         wifiScanResults.forEach { scanResult ->
             val wifiScanResultDTO = WifiScanResultDTO(
-                ssid = scanResult.SSID,
+                ssid = parseSSID(scanResult),
                 bssid = scanResult.BSSID,
                 channel = getWifiChannel(scanResult.frequency),
                 signalLevel = scanResult.level,
-                channelBandwidth = convertToChannelWidth(getWifiChannel(scanResult.frequency))
+                channelBandwidth = convertToChannelWidth(getWifiChannel(scanResult.frequency)),
+                advancedInfo = WifiScanResultDTO.AdvancedInfo(
+                    standard = parseStandard(scanResult),
+                    capabilities = scanResult.capabilities,
+                    bss = isSupportedBT80211v(scanResult), ft = isSupportedFT80211r(scanResult),
+                    rm = isSupportedRM80211k(scanResult),
+                    pmf = isSupportedPMF80211w(scanResult),
+                    ftm = isSupportedFTM80211mc(scanResult),
+                    mlo = isSupportedMLO80211be(scanResult),
+                    mld = parseMLD(scanResult)
+                )
             )
             wifiScanResultList.add(wifiScanResultDTO)
         }
@@ -65,5 +67,175 @@ actual class WifiScanService {
             in 149..165 -> 160 // Channels 149-165 in the 5 GHz band
             else -> -1 // Default case
         }
+    }
+
+    // New Feature --------------------------------------------------------------
+    /**
+     * SSID Parser for given <hidden> network
+     * @hide
+     * Result: "[hidden]" / {SSID}
+     */
+    private fun parseSSID(wifi: ScanResult): String {
+        return if (wifi.SSID.isNullOrBlank()) "[hidden]" else wifi.SSID
+    }
+
+    /**
+     * Wifi's Modem Parser
+     * @hide
+     * Result: "Required API 30+" / {Standard}
+     */
+    private fun parseStandard(wifi: ScanResult): String {
+        // Wi-Fi types
+        val standardMap = mapOf(
+            0 to "Unknown",
+            1 to "Legacy",
+            4 to "802.11n",
+            5 to "802.11ac",
+            6 to "802.11ax",
+            7 to "802.11ad",
+            8 to "802.11be"
+        )
+        // Lookup the standard name based on the ID
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            standardMap.getOrElse(wifi.wifiStandard) { "Unknown" }
+        else "Required API 30+"
+    }
+
+    /**
+     * BSS Transition (BT 802.11v) (May out-of-date: 2012)
+     * @hide
+     * Capabilities contains ["WPA"] & not ["WPA2"]
+     * As ChatGPT: If Fast BSS Transition supported -> BSS Transition supported
+     * Result: -1: ?
+     *          0: Not Supported
+     *          1: Supported
+     */
+    private fun isSupportedBT80211v(wifi: ScanResult): Int {
+        val caps = wifi.capabilities
+        if (isSupportedFT80211r(wifi) == 1) return 1
+        if (caps.contains("WPA2") || caps.contains("WPA3") || caps.contains("RSN")) return 0
+        if (caps.contains("WPA")) return 1
+        return 0
+    }
+
+    /**
+     * Fast BSS Transition (FT 802.11r)
+     * @hide
+     * Capabilities contains ["FT?PSK", "FT?EAP", "FT?SAE"]["+FT]
+     * Result: -1: ?
+     *          0: Not Supported
+     *          1: Supported
+     */
+    private fun isSupportedFT80211r(wifi: ScanResult): Int {
+        val caps = wifi.capabilities
+        if (caps.contains("+FT")) return 1
+        if (Regex("FT[_/+\\-]?(PSK|EAP|SAE)").containsMatchIn(caps)) return 1
+        return 0
+    }
+
+    /**
+     * Protected Management Frames (PMF 802.11w) Supported Check
+     * @hide
+     * SecurityParams.java----------------------------- ID from WifiConfiguration
+     * SECURITY_TYPE_PASSPOINT_R3							12
+     * SECURITY_TYPE_OWE									6
+     * SECURITY_TYPE_EAP_WPA3_ENTERPRISE_192_BIT			5
+     * SECURITY_TYPE_EAP_WPA3_ENTERPRISE					9
+     * SECURITY_TYPE_SAE									4
+     * SECURITY_TYPE_DPP									13
+     *
+     * Or
+     * Capabilities contains ["PMF","MFPR","MFPC"]
+     * Result: -1: Require API 33+
+     *          0: Not Supported
+     *          1: Supported
+     */
+    private val pmfCheckList: IntArray = intArrayOf(4, 5, 6, 9, 12, 13)
+    private fun isSupportedPMF80211w(wifi: ScanResult): Int {
+        if (wifi.capabilities.contains("PMF")) return 1
+        if (wifi.capabilities.contains("MFPR")) return 1
+        if (wifi.capabilities.contains("MFPC")) return 1
+
+        // Additional check by type of Security
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val securityTypes = wifi.securityTypes
+//            var listSecurity = mutableListOf<Int>()
+            for (securityType in securityTypes) {
+//                listSecurity.add(securityType)
+                if (pmfCheckList.contains(securityType)) {
+                    return 1
+                }
+            }
+            return 0
+        }
+        return -1
+    }
+
+    /**
+     * Radio Measurement (RM 802.11k) Supported Check
+     * @hide
+     * In Android, there isn't a direct API through WifiManager
+     * to check specifically for Resource Measurement/Management (RM) support.
+     * RM is a feature that is generally handled at the driver or firmware level of the Wi-Fi chipset,
+     * and it's not directly exposed to app developers through the Android SDK.
+     *
+     * Result: -1: Require API 33+
+     *          0: Not Supported
+     *          1: Supported
+     */
+    private fun isSupportedRM80211k(wifi: ScanResult): Int {
+        if (wifi.capabilities.contains("PRM")) return 1
+        if (wifi.capabilities.contains("RRM")) return 1
+
+        // Additional check by type of Security
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val securityTypes = wifi.securityTypes
+            for (securityType in securityTypes) {
+                if (securityType == 11) {
+                    return 1
+                }
+            }
+            return 0
+        }
+        return -1
+    }
+
+    /** Fine-Time-Measurement (FTM 802.11mc)
+     * @hide
+     * https://developer.android.com/develop/connectivity/wifi/wifi-rtt?hl=en#java
+     * Result: -1: Require API 23+
+     *          0: Not supported
+     *          1: Supported
+     */
+    private fun isSupportedFTM80211mc(wifi: ScanResult): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return if (wifi.is80211mcResponder) 1 else 0
+        } else -1
+    }
+
+    /**
+     * Multi-Link Operation (MLO 802.11be) Supported Check
+     * @hide
+     * Result:  -1: "Required API 33+"
+     *           0: "Not supported"
+     *           1: "Supported"
+     */
+    private fun isSupportedMLO80211be(wifi: ScanResult): Int {
+        // The Multi-Link Operation (MLO) link id for the access point. Only applicable for Wi-Fi 7 access point
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (wifi.apMloLinkId == -1) 0 else 1
+        } else -1
+    }
+
+    /**
+     * Multi-Link Device (MLD) Address
+     * @hide
+     * Result:  "Required API 33+" / {Address}
+     */
+    private fun parseMLD(wifi: ScanResult): String {
+        // The Multi-Link Device (MLD) address of the access point. Only applicable for Wi-Fi 7 access points, null otherwise.
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            "${wifi.apMldMacAddress ?: ""}"
+        } else "Required API 33+"
     }
 }
--- shared/src/commonMain/kotlin/com/ftel/ptnetlibrary/dto/WifiScanResultDTO.kt
+++ shared/src/commonMain/kotlin/com/ftel/ptnetlibrary/dto/WifiScanResultDTO.kt
@@ -1,9 +1,35 @@
 package com.ftel.ptnetlibrary.dto
 
-data class WifiScanResultDTO (
+data class WifiScanResultDTO(
     var ssid: String,
     var bssid: String,
     var channel: Int,
     var signalLevel: Int,
-    var channelBandwidth: Int
-)
+    var channelBandwidth: Int,
+    var advancedInfo: AdvancedInfo? = null
+) {
+    data class AdvancedInfo(
+        val standard: String, // Modem's standard
+        val capabilities: String, // All capabilities
+        // BSS Transition (BT 802.11v): allows switch from one BSS to another BSS
+        //  within the same Extended Service Set (ESS) without losing connectivity
+        val bss: Int,
+        // Fast BSS Transition (FT 802.11r): reducing the time it takes to transition
+        // from one access point (AP) to another within the same Extended Service Set (ESS).
+        val ft: Int,
+        // Radio Measurement (RM 802.11k): enable WiFi clients and access points (APs)
+        // gather information about the radio environment
+        val rm: Int,
+        // Management Frame Protection (MFP 802.11w): a security feature
+        // that enhances the security of wireless networks,
+        val pmf: Int,
+        // Fine Timing Measurement (FTM 802.11mc): used to accurately measure the distance
+        // or range between a client device and an access point (AP).
+        val ftm: Int,
+        // Multi-Link Operation (MLO 802.11be): allows a device to simultaneously communicate
+        // with multiple peers (devices or access points) on different channels within the same band.
+        val mlo: Int,
+        // Multi-Link Device's Address (MLD 802.11be)
+        val mld: String
+    )
+}
